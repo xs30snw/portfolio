7451d9a30fc3bf978ccde1a204720b2d729b3154
xs30snw <e4n22s@gmail.com>
2022-02-23 15:38:49 +0500

    Put components' styles into separate files

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,16 @@
 import { appWithTranslation } from 'next-i18next';
 
 import '../styles/globals.scss';
+import '../styles/header.scss';
+import '../styles/localeSwitcher.scss';
+import '../styles/hero.scss';
+import '../styles/skills.scss';
+import '../styles/techs.scss';
+import '../styles/ctaProjects.scss';
+import '../styles/footer.scss';
 
 function MyApp({ Component, pageProps }) {
-    return (<>        
+    return (<>
         <Component {...pageProps} />
     </>)
 }
