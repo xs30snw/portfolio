233251a3af30bf6809a512b85c3473bd2c24e5b4
xs30snw <e4n22s@gmail.com>
2022-02-24 22:16:18 +0500

    Added images thumbnails to projects

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -1,27 +1,40 @@
+import Image from 'next/image';
+
 function ProjectCard({ project }) {
 
     return (<>
         <article className='project-card'>
+
             <h3 className='project-card__title'>
                 {project.title}
             </h3>
-            
-            <p className='project-card__excerpt'>
-                {project.excerpt}
-            </p>
 
-            <ul className='project-card__links'>
-                <li>
-                    <a className='project-card__link link' href={project.previewLink} target='_blank' rel='noreferrer'>
-                        Live Preview
-                    </a>
-                </li>
-                <li>
-                    <a className='project-card__link link' href={project.sourceLink} target='_blank' rel='noreferrer'>
-                        Source
-                    </a>
-                </li>
-            </ul>
+            <aside className='project-card__aside shadow'>
+                <Image
+                    src={project.thumbnail}
+                    width={900}
+                    height={900}
+                    alt='text' />
+            </aside>
+
+            <section className='project-card__section flex'>
+                <p className='project-card__excerpt'>
+                    {project.excerpt}
+                </p>
+                <ul className='project-card__links'>
+                    <li>
+                        <a className='project-card__link link' href={project.previewLink} target='_blank' rel='noreferrer'>
+                            Live Preview
+                        </a>
+                    </li>
+                    <li>
+                        <a className='project-card__link link' href={project.sourceLink} target='_blank' rel='noreferrer'>
+                            Source
+                        </a>
+                    </li>
+                </ul>
+            </section>
+            
         </article>
     </>)
 }
